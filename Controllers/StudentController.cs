b25f49f5e366517803962f0e152fff754efe0d8f
Prince Magutshwa <princeth.magutshwa@gmail.com>
2024-07-29 16:52:10 +0200

    Initial commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+using WebApi.Models;
+using WebApi.Repository;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<student> GetStudents() {
+            return CollegeRepository.Student;
+        }
+
+        [HttpGet("{id:int}")]
+        public student GetStudentById(int id)
+        {
+            return CollegeRepository.Student.Where(n => n.Id == id).FirstOrDefault();
+        }
+
+
+    }
+}
---
+++ Repository/CollegeRepository.cs
@@ -0,0 +1,21 @@
+﻿using WebApi.Models;
+
+namespace WebApi.Repository
+{
+    public static class CollegeRepository
+    {
+        public static List<student> Student { get; set; } = new List<student>()
+        {
+            new student {
+                Id = 1,
+                Name = "Prince Magutshwa",
+                Email = "p@gmail.com"
+            },
+            new student{
+                Id = 2,
+                Name = "Elon Magutshwa",
+                Email = "e@gmail.com"
+            }
+        };
+    }
+}
